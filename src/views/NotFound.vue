229e09b4a6bf72b2c7f02b0821cde690a0af8708
BarZ <andy820621@gmail.com>
2022-09-13 18:00:12 +0900

    add: NotFound page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+	<section>
+		<BaseCard>
+			<h2>Page not found</h2>
+			<p>
+				This page could not found -- Maybe check out all our
+				<RouterLink to="/coaches"> coaches </RouterLink> page.
+			</p>
+		</BaseCard>
+	</section>
+</template>
+
+<script setup>
+import BaseCard from "../components/ui/BaseCard.vue";
+</script>
+
+<style lang="scss" scoped>
+a {
+	font-size: 1.3rem;
+	font-weight: bold;
+	transition: 0.3s ease-in-out;
+	&:hover {
+		color: #8d64bb;
+	}
+}
+</style>
