a51aec8d2f75bef7f0af2c5ee6e3f54d5aa3bf93
BarZ <andy820621@gmail.com>
2022-09-15 20:11:51 +0900

    update: data

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import BaseCard from "./components/ui/BaseCard.vue";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-// import global UI Async components
-app.component(
-	"BaseCard",
-	defineAsyncComponent(() => import("./components/ui/BaseCard.vue"))
-);
+// Avoid UserAuth refs can't get dom elements because dynamically import BaseCard component.
+app.component("BaseCard", BaseCard);
+// register global UI Async components
 app.component(
 	"BaseButton",
 	defineAsyncComponent(() => import("./components/ui/BaseButton.vue"))
